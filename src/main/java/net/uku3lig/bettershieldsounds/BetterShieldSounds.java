c9611809e25c70ba988e9ed453bd94c8aa5fad57
uku <uku3lig@gmail.com>
2023-02-22 15:07:18 +0100

    initial commit

---
+++ src/main/java/net/uku3lig/bettershieldsounds/BetterShieldSounds.java
@@ -0,0 +1,10 @@
+package net.uku3lig.bettershieldsounds;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterShieldSounds implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
