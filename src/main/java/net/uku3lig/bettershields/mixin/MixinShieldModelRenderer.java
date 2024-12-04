21b0a6491324acba8cfc527420402fbed0361898
uku <hi@uku.moe>
2024-12-04 19:44:43 +0100

    feat: update to 1.21.4

--- src/main/java/net/uku3lig/bettershields/mixin/MixinShieldModelRenderer.java
+++ src/main/java/net/uku3lig/bettershields/mixin/MixinShieldModelRenderer.java
@@ -2,12 +2,12 @@ package net.uku3lig.bettershields.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.item.BuiltinModelItemRenderer;
+import net.minecraft.client.render.item.model.special.ShieldModelRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -19,24 +19,15 @@ import net.uku3lig.bettershields.config.ShieldConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 // part of this code was kindly provided by Marlow's friend, massive thanks to them!
-@Mixin(BuiltinModelItemRenderer.class)
-public class MixinBuiltinModelItemRenderer {
-    @Unique
-    private ModelTransformationMode mode;
-
-    @Inject(method = "render", at = @At("HEAD"))
-    public void getMode(ItemStack stack, ModelTransformationMode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, CallbackInfo ci) {
-        this.mode = mode;
-    }
-
-    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V"))
-    public void changeShieldColor(ModelPart instance, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, Operation<Void> original) {
+@Mixin(ShieldModelRenderer.class)
+public class MixinShieldModelRenderer {
+    @WrapOperation(method = "render(Lnet/minecraft/component/ComponentMap;Lnet/minecraft/item/ModelTransformationMode;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IIZ)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V"))
+    public void changeShieldColor(ModelPart instance, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, Operation<Void> original, @Local(argsOnly = true) ModelTransformationMode mode) {
         ShieldConfig config = BetterShields.getManager().getConfig();
-        if (this.mode.isFirstPerson() && config.isColoredShields()) {
+        if (mode.isFirstPerson() && config.isColoredShields()) {
             if (isDisabled()) {
                 instance.render(matrices, vertices, light, overlay, config.getDisabledColor());
             } else if (isRising()) {
