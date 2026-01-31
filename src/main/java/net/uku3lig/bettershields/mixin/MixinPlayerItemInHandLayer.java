52764ca0dbff1241e0f84a59fc5e1e56ac5ff17c
penguin <penguin586970@gmail.com>
2026-01-30 23:50:09 -0500

    fix: don't assume all avatar entities are players
    this fixes a crash when a mannequin is rendered while holding a shield

--- src/main/java/net/uku3lig/bettershields/mixin/MixinPlayerItemInHandLayer.java
+++ src/main/java/net/uku3lig/bettershields/mixin/MixinPlayerItemInHandLayer.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.uku3lig.bettershields.BetterShields;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +21,9 @@ public class MixinPlayerItemInHandLayer {
     @Inject(method = "submitArmWithItem(Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;Lnet/minecraft/client/renderer/item/ItemStackRenderState;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/HumanoidArm;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;I)V",
     at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/layers/ItemInHandLayer;submitArmWithItem(Lnet/minecraft/client/renderer/entity/state/ArmedEntityRenderState;Lnet/minecraft/client/renderer/item/ItemStackRenderState;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/HumanoidArm;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;I)V"))
     public void fetchCurrentRenderedPlayer(AvatarRenderState avatarRenderState, ItemStackRenderState itemStackRenderState, ItemStack itemStack, HumanoidArm humanoidArm, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int i, CallbackInfo ci) {
-        BetterShields.setCurrentRenderedPlayer((Player) Minecraft.getInstance().level.getEntity(avatarRenderState.id));
+        Level level = Minecraft.getInstance().level;
+        if (level != null && level.getEntity(avatarRenderState.id) instanceof Player player) {
+            BetterShields.setCurrentRenderedPlayer(player);
+        }
     }
 }
